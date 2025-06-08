bdbdf13c764b5c2d99f5395326f6a56667d8f4e7
sarrthak008 <sarthaknavale08@gmail.com>
2025-06-08 12:04:25 +0300

    make api for get 1 user info and post or add new user in our db.json file

--- app/api/users/[id]/route.js
+++ app/api/users/[id]/route.js
@@ -1,5 +1,27 @@
-import { NextResponse } from "next/server"
+import users from "../../../../db.json"
 
-export const  GET =(request,params)=>{
-     return 
+export const  GET =async (request,{params})=>{
+      let { id }=  await params
+      const findedUser =  users.find((user)=>{return id === user._id})
+        if(!findedUser){
+             return Response.json({error:`user with id ${id} not found`},{status:404})
+         }
+    return Response.json({userInfo: findedUser},{status:200})
+
+}
+
+export const PUT =async(request,{params})=>{
+      
+     try {
+         let {id} = await params;
+         let findedUser = users.find((user)=>{return id === user._id})
+          if(!findedUser){
+             return Response
+          }
+
+     } catch (error) {
+        
+     }
+      
+    
 }
--- app/api/users/route.js
+++ app/api/users/route.js
@@ -1,7 +1,32 @@
 import users from "../../../db"
+import { writeFile } from "fs/promises"
 
-
-export const GET=()=>{
-     return Response.json(users)
+export const GET = () => {
+    return Response.json(users)
 }
 
+export const POST = async (request) => {
+    let { age, name, gender, email, phone, about } = await request.json()
+    let requiredFields = { age, name, gender, email, phone, about } // assigning name ,age ets from line 9
+
+    for (const [key, value] of Object.entries(requiredFields)) {  // destructure the array
+        if (!value) {
+            return Response.json({ error: `${key} is required` }, { status: 400 });
+        }
+    }
+
+
+    let newUser = {
+        _id: crypto.randomUUID(),
+        ...requiredFields
+    }
+    users.push(newUser)
+
+    try {
+        await writeFile("db.json", JSON.stringify(users, null, 2))
+        return Response.json({ message: "user added successfully" }, { status: 201 })
+
+    } catch (error) {
+        return Response.json({ error: error.message }, { status: 500 })
+    }
+}
